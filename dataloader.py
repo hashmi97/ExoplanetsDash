21c600aca0fe0c6dd560ee138309713fcf8a7675
Hisham Al Hashmi <46504631+hashmi97@users.noreply.github.com>
2022-08-31 19:41:33 +0200

    updated which files to be dropped

--- dataloader.py
+++ dataloader.py
@@ -35,7 +35,7 @@ class DataLoader():
                      'elon', 'sy_pm', 'sy_pmra', 'sy_pmdec', 'sy_plx', 'sy_bmag', 'sy_vmag', 'sy_jmag', 'sy_hmag',
                      'sy_kmag',
                      'sy_umag', 'sy_gmag', 'sy_rmag', 'sy_imag', 'sy_zmag', 'sy_w1mag', 'sy_w2mag', 'sy_w3mag',
-                     'sy_w4mag',
+                     'sy_w4mag', 'rv_flag', 'pul_flag', 'ptv_flag', 'micro_flag', 'etv_flag', 'ima_flag', 'dkin_flag',
                      'sy_gaiamag', 'sy_icmag', 'sy_tmag', 'sy_kepmag', 'pl_pubdate', 'releasedate', 'pl_nnotes',
                      'st_nphot', 'pl_msinij', 'pl_msinie', 'pl_masse', 'pl_massj', 'st_spectype',
                      'st_nrvc', 'st_nspec', 'pl_nespec', 'pl_ntranspec', 'pl_ratdor', 'rowupdate']
@@ -44,16 +44,22 @@ class DataLoader():
         new_col_names = {'pl_name': 'planetName', 'hostname': 'starName', 'sy_snum': 'numStars',
                          'discoverymethod': 'discoveryMethod', 'disc_year': 'discoveryYear',
                          'disc_locale': 'discoveryLocale',
-                         'disc_facility': 'discoveryFacility', 'rv_flag': 'detectRV', 'pul_flag': 'detectPUL',
-                         'ptv_flag': 'detectPTV', 'tran_flag': 'detectTrans', 'ast_flag': 'detectAST', 'obm_flag': 'detectOBM',
-                         'micro_flag': 'detectMICRO', 'etv_flag': 'detectETV', 'ima_flag': 'detectIMA',
-                         'dkin_flag': 'detectDKIN', 'pl_orbper': 'orbitalPeriod',
+                         'disc_facility': 'discoveryFacility', 'pl_orbper': 'orbitalPeriod',
                          'pl_orbsmax': 'orbitSemiMaj', 'pl_rade': 'planetRadE', 'pl_radj': 'planetRadJ',
                          'pl_masse': 'planetMassE', 'pl_massj': 'planetMassJ', 'pl_dens': 'planetDens',
                          'pl_orbeccen': 'planetEcce', 'pl_eqt': 'planetEqtT',
                          'st_rad': 'starRadius', 'st_mass': 'starMass', 'sy_dist': 'starDistance'}
         self.df.rename(columns=new_col_names, inplace=True)
         self.df.set_index('planetName', drop=True, inplace=True)
+        earth_df = pd.DataFrame.from_dict({"Earth": {'starName': 'Sun', 'numStars': 1, 'discoveryMethod': 'N/A',
+                                             'discoveryYear': np.NaN, 'discoveryLocale': 'N/A',
+                                             'discoveryFacility': 'N/A', 'orbitalPeriod': 365.25,
+                                             'orbitSemiMaj': 1, 'planetRadE': 1, 'planetRadJ': 0.0892,
+                                             'planetDens': 5.51, 'planetEcce': 0.017, 'planetEqtT': 255,
+                                             'starRadius': 1, 'starMass': 1, 'starDistance': 0, 'planetMassJ': 0.003145,
+                                             'planetMassE': 1}}).T
+        self.df = pd.concat([self.df, earth_df])
+        self.df.sort_index(inplace=True)
 
     def get_data(self):
         return self.df
